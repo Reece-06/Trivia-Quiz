4af0f12a4424a9e0d9e4fbfc99c2372720090ab6
Reece De Padua <reecedepadua@gmail.com>
2025-06-01 21:06:25 +0800

    retreiving questions working

--- index.js
+++ index.js
@@ -6,24 +6,38 @@ const port = 3000;
 const API_URL = "https://opentdb.com";
 const API_ENDPOINT = "/api.php";
 triviaGame.use(express.static("public"));
-
+triviaGame.use(express.json());
 triviaGame.get("/", (req, res) => {
   res.render("index.ejs");
 });
-triviaGame.get("/api/getQuestions", async (req, res) => {
-  const amount = req.query.amount || 10;
+// triviaGame.get("/api/getQuestions", async (req, res) => {
+//   const amount = req.query.amount || 10;
 
-  try {
-    const result = await axios.get(
-      `${API_URL}${API_ENDPOINT}?amount=${amount}`
-    );
+//   try {
+//     const result = await axios.get(
+//       `${API_URL}${API_ENDPOINT}?amount=${amount}`
+//     );
 
-    // res.render("index.ejs");
-    console.log(result.data);
-    console.log(res.status);
-  } catch (error) {
-    console.log(error);
-  }
+//     // res.render("index.ejs");
+//     console.log(result.data);
+//     console.log(res.status);
+//   } catch (error) {
+//     console.log(error);
+//   }
+// });
+triviaGame.post("/questions", async (req, res) => {
+  const gameSettings = req.body;
+
+  const data = {
+    amount: gameSettings.numQuestions,
+    category: gameSettings.category,
+    difficulty: gameSettings.dificulty,
+  };
+  console.log(data);
+  const URL = `${API_URL}${API_ENDPOINT}?amount=${data.amount}&category=${data.category}&difficulty=${data.difficulty}&type=multiple`;
+  const response = await axios.get(URL);
+  console.log(response.data.results);
+  res.render("index.ejs", data);
 });
 triviaGame.listen(port, () => {
   console.log(`Server is running on port ${port}`);
--- public/scripts/game-settings.js
+++ public/scripts/game-settings.js
@@ -1,4 +1,19 @@
 const form = document.querySelector(".form");
+// Send data to server
+const sendDataToServer = async (data) => {
+  try {
+    const response = await fetch("/questions", {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    console.log("Server response:", response.data);
+  } catch (error) {
+    console.log(error);
+  }
+};
 // Handle a form submit
 const handleFormSubmit = (e) => {
   e.preventDefault();
@@ -8,6 +23,7 @@ const handleFormSubmit = (e) => {
   console.log(formData.entries());
   const data = Object.fromEntries(formData.entries());
   console.log(data);
+  sendDataToServer(data);
 };
 
 form.addEventListener("submit", handleFormSubmit);
