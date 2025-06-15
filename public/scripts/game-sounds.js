e921e4ebaaa22f68ca55253597c250ce9eee7175
Reece De Padua <reecedepadua@gmail.com>
2025-06-15 13:40:09 +0800

    added correct and wrong answer sounds

--- public/scripts/game-sounds.js
+++ public/scripts/game-sounds.js
@@ -8,9 +8,17 @@ const addButtonsEvtListener = () => {
 // Play the click sound of buttons
 const playClickSound = (e) => {
   const clickSound = new Audio("../sounds/clickSound.mp3");
+  const correctClickSound = new Audio("../sounds/correctAnswer.mp3");
+  const wrongClickSound = new Audio("../sounds/wrongSound.mp3");
+
   const isMainButton = e.target.classList.contains("main-button");
+  const isCorrect = e.target.dataset.correct;
   if (!isMainButton) {
     clickSound.play();
+  } else if (isMainButton && isCorrect) {
+    correctClickSound.play();
+  } else {
+    wrongClickSound.play();
   }
 };
 
