229f90745cfbd27a32bfa9e3a1ca19d4bf989de2
Reece De Padua <reecedepadua@gmail.com>
2025-06-17 11:31:09 +0800

    fixed layout shift

--- public/scripts/game-sounds.js
+++ public/scripts/game-sounds.js
@@ -1,3 +1,7 @@
+const clickSound = new Audio("../sounds/clickSound.mp3");
+const correctClickSound = new Audio("../sounds/correctAnswer.mp3");
+const wrongClickSound = new Audio("../sounds/wrongSound.mp3");
+
 // Event listeners for buttons
 const addButtonsEvtListener = () => {
   const buttons = document.querySelectorAll("button");
@@ -7,13 +11,14 @@ const addButtonsEvtListener = () => {
 };
 // Play the click sound of buttons
 const playClickSound = (e) => {
-  const clickSound = new Audio("../sounds/clickSound.mp3");
-  const correctClickSound = new Audio("../sounds/correctAnswer.mp3");
-  const wrongClickSound = new Audio("../sounds/wrongSound.mp3");
-
   const isMainButton = e.target.classList.contains("main-button");
   const isCorrect = e.target.dataset.correct;
+
   if (!isMainButton) {
+    correctClickSound.pause();
+    correctClickSound.currentTime = 0;
+    wrongClickSound.pause();
+    wrongClickSound.currentTime = 0;
     clickSound.play();
   } else if (isMainButton && isCorrect) {
     correctClickSound.play();
